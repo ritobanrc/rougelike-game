f90c6ca99dca35968638bfae44417f1b398d092d
TurtleGameDev <theturtleofawesomeness@gmail.com>
2017-10-20 06:55:08 -0700

    Added basic player movement

---
+++ Rougelike Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move (float h, float v)
+    {
+        movement.Set(h, v, 0f);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
