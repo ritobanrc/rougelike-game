a725e69e6eb40b9d550156d777af9a5ce1e27dea
ritoban123 <ritobanrc@gmail.com>
2017-10-21 09:33:49 -0700

    made player movement tile-based

---
+++ Roguelike Game/Assets/Scripts/Coord.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coord
+{
+    public int X { get; protected set; }
+    public int Y { get; protected set; }
+    public Vector2 AsV2
+    {
+        get
+        {
+            return new Vector2(this.X, this.Y);
+        }
+    }
+
+    public Coord(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public static Coord operator +(Coord a, Coord b)
+    {
+        return new Coord(a.X + b.X, a.Y + b.Y);
+    }
+
+    public override string ToString()
+    {
+        return "Coord: (" + this.X + ", " + this.Y + ")";
+    }
+}
--- Roguelike Game/Assets/Scripts/PlayerMovement.cs
+++ Roguelike Game/Assets/Scripts/PlayerMovement.cs
@@ -2,33 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Coord PlayerPosition { get; protected set; }
 
     public float speed = 6f;
+    public float waitAfterMove = 0.01f;
 
-    Vector3 movement;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private bool currentlyMoving;
 
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
+        PlayerPosition = new Coord(0, 0);
     }
 
     private void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        int h = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        int v = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
 
-        Move(h, v);
+        if (currentlyMoving == false && (Mathf.Abs(h) + Mathf.Abs(v)) == 1)
+            StartCoroutine(Move(h, v));
     }
 
-    void Move (float h, float v)
+    private IEnumerator Move(int h, int v)
     {
-        movement.Set(h, v, 0f);
-
-        movement = movement.normalized * speed * Time.deltaTime;
-
-        rb.MovePosition(transform.position + movement);
+        PlayerPosition += new Coord(h, v);
+        currentlyMoving = true;
+        Debug.Log("Started Move to Position: " + PlayerPosition.ToString());
+        while((PlayerPosition.AsV2 - rb.position).sqrMagnitude > 0.02)
+        {
+            rb.MovePosition(rb.position + new Vector2(h, v) * speed * Time.deltaTime);
+            yield return null;
+        }
+        rb.MovePosition(PlayerPosition.AsV2);
+        Debug.Log("Move Completed. PlayerPosition: " + PlayerPosition.ToString());
+        yield return new WaitForSeconds(waitAfterMove);
+        currentlyMoving = false;
     }
 }
