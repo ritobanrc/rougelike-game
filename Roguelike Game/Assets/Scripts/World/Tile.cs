d70ddd793b907b2302a67b80decea817984bb5fd
Ritoban-Desktop <ritobanrc@gmail.com>
2017-10-31 19:17:37 -0700

    basic tile stuff

--- Roguelike Game/Assets/Scripts/World/Tile.cs
+++ Roguelike Game/Assets/Scripts/World/Tile.cs
@@ -7,5 +7,25 @@ using UnityEngine;
 /// </summary>
 public class Tile 
 {
-
+    /// <summary>
+    /// The Tile Position
+    /// </summary>
+    public Coord Position { get; protected set; }
+    /// <summary>
+    /// Returns the tile X position
+    /// </summary>
+    public int X { get { return Position.X; } }
+    /// <summary>
+    /// Returns the tile Y position
+    /// </summary>
+    public int Y { get { return Position.Y; } }
+    /// <summary>
+    /// Creates a new tile
+    /// </summary>
+    /// <param name="x">The X coordinate of the new tile</param>
+    /// <param name="y">The Y coordinate of the new tile</param>
+    public Tile(int x, int y)
+    {
+        this.Position = new Coord(x, y);
+    }
 }
