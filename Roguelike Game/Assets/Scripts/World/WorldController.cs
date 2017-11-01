dd1aaf9b5006bfd9fcd6b2817b2cc01882a4c724
Ritoban-Desktop <ritobanrc@gmail.com>
2017-10-31 19:14:06 -0700

    Re-organized the files.

---
+++ Roguelike Game/Assets/Scripts/World/WorldController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A Global Component holding information about the world
+/// </summary>
+public class WorldController : MonoBehaviour
+{
+    /// <summary>
+    /// The whole world.
+    /// </summary>
+    public World World { get; protected set; }
+}
