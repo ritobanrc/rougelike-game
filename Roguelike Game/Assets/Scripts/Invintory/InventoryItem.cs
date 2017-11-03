1815c97a5d1319522e9b6c665984a0eebd787d6f
TurtleGameDev <theturtleofawesomeness@gmail.com>
2017-11-02 20:54:40 -0700

    Added Code to Inventory class and mangagar

--- Roguelike Game/Assets/Scripts/Invintory/InventoryItem.cs
+++ Roguelike Game/Assets/Scripts/Invintory/InventoryItem.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class InventoryItem
 {
     /// <summary>
-    /// This integer is the slot number in the player's inventory
+    /// This integer is the item type
     /// </summary>
-    public int Slot { get; protected set; }
-    public InventoryItem (int slot)
+    public int item { get; protected set; }
+
+    /// <summary>
+    /// Amount of items stacked
+    /// </summary>
+    public int stack { get; protected set; }
+
+    // Returns the item in the slot as a float
+    public int itemAsi()
     {
-        this.Slot = slot;
+        return this.item;
+    }
+
+    // Returns the amount of items stacked
+    public int stackAsi()
+    {
+        return this.item;
+    }
+
+    public void AddToStack(int amount = 1)
+    {
+        this.stack += amount;
+    }
+
+    public void RemoveFromStack(int amount = 1)
+    {
+        this.stack -= amount;
+    }
+
+    public InventoryItem (int item)
+    {
+        this.item = item;
+        this.stack = 1;
     }
 }
--- Roguelike Game/Assets/Scripts/Invintory/InvintoryMangager.cs
+++ Roguelike Game/Assets/Scripts/Invintory/InvintoryMangager.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class InvintoryMangager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// List of the items in 
+    /// </summary>
+    private List<InventoryItem> inventory = new List<InventoryItem>();
+
+    /// <summary>
+    /// Defines a size for the inventory
+    /// </summary>
+    public int InventorySize = 40;
+
+    /// <summary>
+    /// Defines the size of the stack
+    /// </summary>
+    public int StackSize = 64;
+
+    public bool AddItem(int itemType, int itemNub = 1)
+    {   
+        for (int i = 0; i < InventorySize; i++)
+        {
+            if (inventory[i] != null)
+            {
+                if (inventory[i].itemAsi() == itemType)
+                {
+                    if (inventory[i].stackAsi() < StackSize)
+                    {
+                        inventory[i].AddToStack(itemNub);
+                        return true;      
+                    }
+                }
+            } else break;
+        }
+        if (inventory.Count < InventorySize) return false;
+        inventory.Add(new InventoryItem(itemType));
+        return true;
+    }
+    public bool RemoveItem(int itemType, int itemNum = 1)
+    {
+        for (int i = 0; i < InventorySize; i++)
+        {
+            if (inventory[i] != null)
+            {
+                if (inventory[i].itemAsi() == itemType)
+                {
+                    if (inventory[i].stackAsi() < StackSize)
+                    {
+                        inventory[i].RemoveFromStack(itemNum);
+                        return true;
+                    }
+                }
+            }
+            else break;
+            }
+        return false;
+    }
 }
