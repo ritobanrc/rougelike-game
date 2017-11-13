7b154f8055fe1ca072037b426899c2c58d191f2e
Ritoban-Desktop <ritobanrc@gmail.com>
2017-11-12 18:46:40 -0800

    Some preliminary work done before diving into the WorldImporter code. Commiting so I can revert when I break everything.

---
+++ Roguelike Game/Assets/Scripts/World/TileTypesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+
+// A lot in common with the Furniture Controller. Should we make XMLImporter an abstract class?
+public class TileTypesController : MonoBehaviour {
+
+    public string TileTypesFileName = "Data/TileTypes.xml";
+
+    public List<TileType> TileTypes { get; private set; }
+
+
+    private void Awake()
+    {
+        ReadTileTypes();
+    }
+
+    private void ReadTileTypes()
+    {
+        Debug.Log("Reading all TileTypes from " + TileTypesFileName);
+        TileTypes = new List<TileType>();
+        XmlDocument xmlDoc = new XmlDocument();
+        string filePath = Path.Combine(Application.streamingAssetsPath, TileTypesFileName);
+        if (filePath.Contains("://"))
+            throw new XmlException("How the actual f*ck did you manage to run this on Web/Mobile/Not Standalone?");
+        xmlDoc.Load(filePath);
+        int id = 0;
+        foreach (XmlNode xmlNode in xmlDoc.DocumentElement)
+        {
+            /// TODO: This is not scalable! Is there a better way to do this?
+            // For each furniture listed, get its information
+            string name = xmlNode.Attributes["name"].Value;
+            Color lvlImageColor = Color.black;
+            ColorUtility.TryParseHtmlString(xmlNode.SelectSingleNode("LvlImgColor").InnerText, out lvlImageColor);
+            Color32 lvlImageColor32 = (Color32)lvlImageColor;
+            // Furniture.Graphics.ImagePath - TODO: Handle more complicated graphics
+            string imagePath = xmlNode.SelectSingleNode("Graphics").SelectSingleNode("ImagePath").InnerText;
+            TileType type = new TileType(name, id);
+            type.LvlImgColor = lvlImageColor32;
+
+            TileTypes.Add(type);
+            //WorldImporter.FurnColorToProto.Add(lvlImageColor32, prototype);
+            id++;
+        }
+    }
+}
--- Roguelike Game/Assets/Scripts/World/WorldGameObjectCreator.cs
+++ Roguelike Game/Assets/Scripts/World/WorldGameObjectCreator.cs
@@ -33,13 +33,24 @@ public class WorldGameObjectCreator : MonoBehaviour
                 sr.sprite = _TEMP_EmptyTileSprite; // Get more tiletypes
                 // TODO: I don't like how this is setup. What if we decide to add more stuff to our tile?
                 // Shouldn't this be in the Furniture controller?
+                // At the very least, move this into it's own function. But that's for future me. Present me is not sure how he's going to complete this massive reworking. Even though it's actually not that big. 
                 if(t.Furniture != null)
                 {
                     Sprite s = Resources.Load<Sprite>(t.Furniture.Prototype.ImagePath);
                     Debug.Log(t.Furniture.Prototype.ImagePath);
                     GameObject furnObj = Instantiate(EmptyFurniturePrefab, obj.transform, false);
                     furnObj.GetComponent<SpriteRenderer>().sprite = s;
-                    
+                    if(t.Furniture.Prototype.hasCol)
+                    {
+                        BoxCollider2D bc2d = obj.AddComponent<BoxCollider2D>();
+                    }
+                    if (t.Furniture.Prototype.hasRb)
+                    {
+                        Rigidbody2D rb = obj.AddComponent<Rigidbody2D>();
+                        // Because this is furniture, we are assuming the rigidbody to be static. 
+                        rb.isKinematic = true;
+                        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+                    }
                 }
             }
         }
