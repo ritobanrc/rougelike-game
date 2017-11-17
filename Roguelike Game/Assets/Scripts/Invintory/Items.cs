1b88d0fa03adb2bb89001f6b787277eb909b5280
TurtleGameDev <theturtleofawesomeness@gmail.com>
2017-11-16 19:15:28 -0800

    Added more art and stuff

---
+++ Roguelike Game/Assets/Scripts/Invintory/Items.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items {
+
+    public enum items { Steel, Gold, blueSword, redSword, purpleSword };
+    public int[] stack;
+
+	// Use this for initialization
+	public Items () {
+        stack = new int[5];
+        stack[0] = 128;
+        stack[1] = 128;
+        stack[2] = 1;
+        stack[3] = 1;
+        stack[4] = 1;
+	}
+}
