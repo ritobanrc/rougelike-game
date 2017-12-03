6f30d29fa7ddd78d30faa65e96b4329003fe0453
TurtleGameDev <theturtleofawesomeness@gmail.com>
2017-12-02 17:18:49 -0800

    Made a prefab spawner so we won't get any more merge errors

---
+++ Roguelike Game/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + new Vector3(0f,0f,-10f);
+	}
+}
---
+++ Roguelike Game/Assets/Scripts/Sceen Spawner/SceenSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceenSpawner : MonoBehaviour
+{
+    private void Start()
+    {
+        Object player = Instantiate(Resources.Load("Player/Player"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        player.name = "Player";
+        Object mainCamera = Instantiate(Resources.Load("Camera/Main Camera"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        mainCamera.name = "Main Camera";
+        Object canvas = Instantiate(Resources.Load("Inventory/UICanvas"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        canvas.name = "UICanvas";
+        Object UpdateManager = Instantiate(Resources.Load("Misc/UpdateManager"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        UpdateManager.name = "UpdateManager";
+        Object EventSystem = Instantiate(Resources.Load("Inventory/EventSystem"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        EventSystem.name = "EventSystem";
+        Object WorldImporter = Instantiate(Resources.Load("WorldGen/World Importer"), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        WorldImporter.name = "World Importer";
+        GameObject.Destroy(this.transform.gameObject);
+    }
+}
